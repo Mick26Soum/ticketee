cef94f4138ab64de11e524d2d37bfd2ee2c2e276
Mick Soum <mick26soum@gmail.com>
2013-03-09 13:39:41 -0800

    Associate users to tickets when tickets are created

--- spec/integration/creating_tickets_spec.rb
+++ spec/integration/creating_tickets_spec.rb
@@ -2,18 +2,31 @@ require "spec_helper"
 
 feature "Creating Tickets" do
 
-  before {
+  before do
     Factory(:project, :name => "Internet Explorer")
+    user = Factory(:user, :email => "ticketee@example.com")
+    user.confirm!
+
     visit '/'
     click_link 'Internet Explorer'
     click_link 'New Ticket'
-  }
+    message = "You need to sign in or sign up before continuing."
+    page.should have_content(message)
+
+    fill_in "Email", :with => "ticketee@example.com"
+    fill_in "Password", :with => "password"
+    click_button "Sign in"
+    within("h2") {page.should have_content("New Ticket")}
+  end
 
   scenario 'Creating a ticket' do
     fill_in "Title", :with => "Non-standard compliance"
     fill_in "Description", :with => "My pages are ugly!"
     click_button "Create Ticket"
     page.should have_content("Ticket has been created")
+      within("#ticket #author") do
+      page.should have_content("Created by ticketee@example.com")
+    end
 
   end
 
@@ -22,6 +35,7 @@ feature "Creating Tickets" do
     page.should have_content("Ticket has not been created")
     page.should have_content("Title can't be blank")
     page.should have_content("Description can't be blank")
+
   end
 
   scenario "Description must be longer than 10 characters" do
@@ -31,5 +45,7 @@ feature "Creating Tickets" do
   page.should have_content("Ticket has not been created")
   page.should have_content("Description is too short")
   end
+
+
 
 end
